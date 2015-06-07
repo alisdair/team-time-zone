7cba4f9a7f31f2cf662b0d46601bb689add56a51
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-06-07 21:21:33 +0100

    Add mirage route for Slack user.info endpoint
    This is enough to support a very basic custom Ember Data adapter.

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,15 +1,34 @@
 import Ember from 'ember';
 
+function invalidTokenResponse(token) {
+  if (Ember.isNone(token)) {
+    return { ok: false, error: 'not_authed' };
+  }
+
+  if (token === 'invalid') {
+    return { ok: false, error: 'invalid_auth' };
+  }
+}
+
 export default function() {
   this.get('/slack/users.list', function(db, request) {
-    if (Ember.isNone(request.params.token)) {
-      return { ok: false, error: 'not_authed' };
-    }
-
-    if (request.params.token === 'invalid') {
-      return { ok: false, error: 'invalid_auth' };
-    }
+    let response = invalidTokenResponse(request.params.token);
+    if (!Ember.isNone(response)) { return response; }
 
     return { ok: true, members: db.users };
+  });
+
+  this.get('/slack/users.info', function(db, request) {
+    let response = invalidTokenResponse(request.params.token);
+    if (!Ember.isNone(response)) { return response; }
+
+    let user = db.users.find(request.params.user);
+    if (Ember.isNone(user)) {
+      return { ok: false, error: 'user_not_found' };
+    }
+
+    // FIXME: no support for errors 'user_not_visible' and 'account_inactive'
+
+    return { ok: true, user: user };
   });
 }
