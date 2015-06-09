080c20e6df7006763db29709bc5f78919c943d40
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-06-09 01:12:04 +0100

    Update user model and template

--- app/models/user.js
+++ app/models/user.js
@@ -2,9 +2,16 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
+
   deleted: DS.attr(),
+
+  firstName: DS.attr(),
+  lastName: DS.attr(),
   realName: DS.attr(),
+
   tz: DS.attr(),
   tzLabel: DS.attr(),
-  tzOffset: DS.attr()
+  tzOffset: DS.attr(),
+
+  image_72: DS.attr(),
 });
