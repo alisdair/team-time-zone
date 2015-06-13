42edfd02f829cdfbf4eb40cd978882e0c9c07083
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-06-14 00:51:21 +0100

    Mirage: use query params, not params
    Our params are coming from the URL query string, not the request body.

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -12,17 +12,17 @@ function invalidTokenResponse(token) {
 
 export default function() {
   this.get('/slack/users.list', function(db, request) {
-    let response = invalidTokenResponse(request.params.token);
+    let response = invalidTokenResponse(request.queryParams.token);
     if (!Ember.isNone(response)) { return response; }
 
     return { ok: true, members: db.users };
   });
 
   this.get('/slack/users.info', function(db, request) {
-    let response = invalidTokenResponse(request.params.token);
+    let response = invalidTokenResponse(request.queryParams.token);
     if (!Ember.isNone(response)) { return response; }
 
-    let user = db.users.find(request.params.user);
+    let user = db.users.find(request.queryParams.user);
     if (Ember.isNone(user)) {
       return { ok: false, error: 'user_not_found' };
     }
