3209742f37cc9ba27b9ff3bcd145c004cd9464e6
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-07-03 23:14:42 +0100

    Friendly display of user timezone offset
    Should probably not be in the model, but oh well.

--- app/models/user.js
+++ app/models/user.js
@@ -15,7 +15,14 @@ export default DS.Model.extend({
   tzOffset: DS.attr(),
 
   timezone: Ember.computed('tzOffset', function() {
-    return this.get('tzOffset') / 3600;
+    let tz = this.get('tzOffset');
+    let sign = tz < 0 ? '-' : '+';
+    let offset = Math.abs(tz);
+    let hours = Math.trunc(offset / 3600);
+    let minutes = (offset % 3600) / 60;
+    let pad = i => (`0${i}`).slice(-2);
+
+    return `${sign}${pad(hours)}:${pad(minutes)}`;
   }),
 
   image_72: DS.attr() // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
