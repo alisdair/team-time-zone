e8e868575f8d28aefe12898934dbfe1b8babd90b
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-07-05 13:56:42 +0100

    Add acceptance test for showing users

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -22,7 +22,7 @@ test('with no users, shows message', function(assert) {
   visit('/');
 
   andThen(function() {
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/', 'shows index page');
 
     assert.equal(find('.user-profile').length, 0, 'shows no users');
 
@@ -31,7 +31,28 @@ test('with no users, shows message', function(assert) {
   });
 });
 
-skip('shows a list of users grouped by timezone', function(assert) {
+test('shows a list of users grouped by timezone', function(assert) {
+  assert.expect(4);
+
+  authenticateSession();
+
+  let tzOffsets = [-25200, -25200, 3600, 7200, 7200];
+  let users = tzOffsets.map(tzo => server.create('user', { 'tz_offset': tzo }));
+
+  visit('/');
+
+  andThen(function() {
+    assert.equal(find('.user-profile').length, 5, 'shows 5 user profiles');
+
+    assert.equal(find('.timezone-group').length, 3,
+                 'users grouped into 3 timezones');
+
+    assert.equal(find('.timezone-column').length, 10,
+                 'shows 11 total timezone columns');
+
+    assert.equal(find('.timezone-column--empty').length, 7,
+                 '8 of the timezone columns are empty');
+  });
 });
 
 skip('distant timezones have more space between them', function(assert) {
