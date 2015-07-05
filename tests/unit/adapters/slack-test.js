a89eb8191e7149f3b054cf9ec2a54b751365fd1a
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-07-05 13:38:39 +0100

    Use Pretender for Slack adapter tests
    Stubbing the Ember.$.ajax method was unreliable, and Pretender does the
    job very well anyway.
    
    Also upgrade ember-simple-auth to prepare for acceptance testing.

--- tests/unit/adapters/slack-test.js
+++ tests/unit/adapters/slack-test.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
+import Pretender from 'pretender';
 
 moduleFor('adapter:slack', 'Unit | Adapter | slack');
+
+const JSON_HEADERS = { 'Content-Type': 'application/json' };
 
 let user = { modelName: 'user' };
 
@@ -81,99 +84,75 @@ test('findAll with stubbed ajax method', function(assert) {
 });
 
 test('ajax with successful response', function(assert) {
-  assert.expect(7);
-  let done = assert.async();
+  assert.expect(2);
 
   let adapter = this.subject();
 
-  let body = { ok: true, user: 'stub' };
+  let body = JSON.stringify({ ok: true, user: 'stub' });
 
-  let originalAjax = Ember.$.ajax;
-  Ember.$.ajax = function(hash) {
-    assert.equal(hash.url, '/url', 'sets URL');
-    assert.equal(hash.type, 'GET', 'sets request type');
-    assert.deepEqual(hash.data, { data: 'here' }, 'sets request body');
-    assert.equal(hash.dataType, 'json', 'sets request data type');
-    assert.equal(typeof hash.success, 'function', 'success is a function');
-    assert.equal(typeof hash.error, 'function', 'error is a function');
-
-    hash.success(body);
-  };
-
-  try {
-    Ember.run(function() {
-      let result = adapter.ajax('/url', 'GET', { data: 'here' });
-
-      result.then(function(response) {
-        assert.deepEqual(response, body, 'should resolve with stubbed body');
-        done();
-      }, function() {
-        assert.ok(false, 'should not reject promise');
-        done();
-      });
+  let server = new Pretender(function() {
+    this.get('/url', function(request) {
+      assert.equal(request.queryParams.data, 'here', 'sends request data');
+      return [200, JSON_HEADERS, body];
     });
-  } finally {
-    Ember.$.ajax = originalAjax;
-  }
+  });
+
+  Ember.run(function() {
+    let result = adapter.ajax('/url', 'GET', { data: 'here' });
+
+    result.then(function(response) {
+      let json = JSON.parse(body);
+      assert.deepEqual(response, json, 'should resolve with body');
+    }, function() {
+      assert.ok(false, 'should not reject promise');
+    });
+  });
 });
 
 test('ajax with successful response but { ok: false }', function(assert) {
   assert.expect(1);
-  let done = assert.async();
-
   let adapter = this.subject();
 
-  let body = { ok: false, error: 'whatever' };
+  let body = JSON.stringify({ ok: false, error: 'whatever' });
 
-  let originalAjax = Ember.$.ajax;
-  Ember.$.ajax = function(hash) {
-    hash.success(body);
-  };
-
-  try {
-    Ember.run(function() {
-      let result = adapter.ajax('/url', 'GET');
-
-      result.then(function() {
-        assert.ok(false, 'should not resolve promise');
-        done();
-      }, function(error) {
-        assert.deepEqual(error, body, 'should reject with stubbed body');
-        done();
-      });
+  let server = new Pretender(function() {
+    this.get('/url', function(request) {
+      return [200, JSON_HEADERS, body];
     });
-  } finally {
-    Ember.$.ajax = originalAjax;
-  }
+  });
+
+  Ember.run(function() {
+    let result = adapter.ajax('/url', 'GET');
+
+    result.then(function() {
+      assert.ok(false, 'should not resolve promise');
+    }, function(error) {
+      let json = JSON.parse(body);
+      assert.deepEqual(error, json, 'should reject with body');
+    });
+  });
 });
 
 test('ajax with error response', function(assert) {
-  assert.expect(2);
-  let done = assert.async();
-
+  assert.expect(1);
   let adapter = this.subject();
 
-  let jqXHR = { then: x => x, message: 'whatever' };
+  let body = JSON.stringify({ message: 'whatever' });
 
-  let originalAjax = Ember.$.ajax;
-  Ember.$.ajax = function(hash) {
-    hash.error(jqXHR);
-  };
-
-  try {
-    Ember.run(function() {
-      let result = adapter.ajax('/url', 'GET');
-
-      result.then(function() {
-        assert.ok(false, 'should not resolve promise');
-        done();
-      }, function(error) {
-        assert.equal(error.message, 'whatever', 'should reject with error');
-        assert.equal(error.then, null, 'removes the jqXHR then function');
-        done();
-      });
+  let server = new Pretender(function() {
+    this.get('/url', function(request) {
+      return [500, JSON_HEADERS, body];
     });
-  } finally {
-    Ember.$.ajax = originalAjax;
-  }
+  });
+
+  Ember.run(function() {
+    let result = adapter.ajax('/url', 'GET');
+
+    result.then(function() {
+      assert.ok(false, 'should not resolve promise');
+    }, function(error) {
+      let json = error.responseJSON;
+      assert.equal(json.message, 'whatever', 'should reject with error');
+    });
+  });
 });
