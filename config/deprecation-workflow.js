d42c7054b079603c17dfce1fcfe68b106debb329
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2016-06-23 13:46:06 +0100

    Dependency fiddling

--- config/deprecation-workflow.js
+++
@@ -1,6 +0,0 @@
-window.deprecationWorkflow = window.deprecationWorkflow || {};
-window.deprecationWorkflow.config = {
-  workflow: [
-    { handler: "silence", matchId: "ember-metal.merge" }
-  ]
-};
